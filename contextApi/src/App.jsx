78c691d690875876e4c82672304e859369663511
Sahil Vani <sahilvani007@gmail.com>
2024-05-01 14:20:15 +0530

    updated react folder according to deployment

--- contextApi/src/App.jsx
+++ contextApi/src/App.jsx
@@ -1,7 +1,7 @@
 import UserContextProvider from "./context/UserContextProvider";
 
 import "./App.css";
-import Login from "./components/login";
+import Login from "./components/Login";
 import Profile from "./components/Profile";
 
 function App() {
--- currency/src/App.jsx
+++ currency/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
 
   return (
     <div
-      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+      className="w-auto h-screen flex  justify-center items-center bg-cover bg-no-repeat"
       style={{
         backgroundImage: `linear-gradient(to top, #a18cd1 0%, #fbc2eb 100%)`,
       }}
